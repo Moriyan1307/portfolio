70d5c5d5f080a7a62e5e63f12cba4365ff5f404c
Moriyan1307 <aaryanmori@gmail.com>
2025-06-24 20:03:37 -0500

    Added work

---
+++ src/app/work/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Work",
+  description: "Work",
+};
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Work</h1>
+      <p className="mb-4">Coming soon ...</p>
+    </section>
+  );
+}
